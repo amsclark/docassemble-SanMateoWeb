da35d7411b87c219d731628a7d56b9f90691bb4d
System Administrator <amsclark@gmail.com>
2025-07-16 05:14:10 -0700

    2nd commit

--- setup.py
+++ setup.py
@@ -44,7 +44,7 @@ def find_package_data(where='.', package='', exclude=standard_exclude, exclude_d
     return out
 
 setup(name='docassemble.SanMateoWeb',
-      version='0.0.60',
+      version='0.0.61',
       description=('A docassemble extension.'),
       long_description='# docassemble.SanMateoWeb\r\n\r\nA docassemble extension.\r\n\r\n## Author\r\n\r\nSystem Administrator, admin@admin.com\r\n\r\n',
       long_description_content_type='text/markdown',
